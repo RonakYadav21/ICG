a194202de5894a41cf3a87a51cdf3ef61145346f
ronakyadav21 <ronakyadav9977@gmail.com>
2025-09-17 17:12:34 +0530

    API-GATEWAY and AUTH-SERVIC implemented

---
+++ Backend/Auth-Service/src/main/java/com/auth_service/DTO/RegisterAuthUserRequest.java
@@ -0,0 +1,10 @@
+package com.auth_service.DTO;
+
+import lombok.Data;
+
+@Data
+public class RegisterAuthUserRequest {
+ private String username;
+ private String password;
+ private String role;
+}
